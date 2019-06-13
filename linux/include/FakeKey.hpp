c1ece1ec8e5ed22f52d59e8b318f898a77b8b0f3
krisukox <krzysiula1313@gmail.com>
2019-06-13 22:32:34 +0200

    Linux support for every letter on keyboard

---
+++ linux/include/FakeKey.hpp
@@ -0,0 +1,11 @@
+#include <X11/Xlib.h>
+
+class FakeKey
+{
+public:
+    FakeKey();
+    ~FakeKey();
+    void pressKey(u_int16_t keyId);
+private:
+    Display *display;
+};
---
+++ linux/src/FakeKey.cpp
@@ -0,0 +1,25 @@
+#include "../include/FakeKey.hpp"
+#include <X11/keysym.h>
+#include <X11/extensions/XTest.h>
+
+//XTestFakeButtonEvent(display, 3, True, CurrentTime); //Mouse down
+//XTestFakeButtonEvent(display, 3, False, CurrentTime); //Mouse up
+
+FakeKey::FakeKey()
+{
+    display = XOpenDisplay(NULL);
+}
+
+FakeKey::~FakeKey()
+{
+    XCloseDisplay(display);
+}
+
+void FakeKey::pressKey(u_int16_t keyId)
+{
+//        XK_KP_Enter
+    KeyCode keycode = XKeysymToKeycode(display, keyId);
+    XTestFakeKeyEvent(display, keycode, True, CurrentTime);
+    XTestFakeKeyEvent(display, keycode, False, CurrentTime);
+    XFlush(display);
+}
