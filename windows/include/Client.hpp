52535d8275e9783e67647ceb3df1ca60423c3f31
krisukox <krzysiula1313@gmail.com>
2019-07-02 19:32:39 +0200

    [windows]Port in progress

--- windows/include/Client.hpp
+++ windows/include/Client.hpp
@@ -1,30 +1,38 @@
 #pragma once
 
-#include <WinSock2.h>
-#include <memory>
-#include <string>
-#include <ws2tcpip.h>
-
+#include <boost/asio.hpp>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <thread>
 #include "Console.hpp"
 #include "Keyboard.hpp"
+#include "chat_message.hpp"
 
-#pragma comment(lib, "ws2_32.lib")
+using boost::asio::ip::tcp;
+typedef std::deque<chat_message> chat_message_queue;
 
-class Client
+class chat_client
 {
 public:
-    Client();
+    chat_client(boost::asio::io_context&, const tcp::resolver::results_type&);
+    void write(const chat_message& msg);
+    void close();
 
 private:
-    void initializeWinSock();
-    void createSocket();
-    void connctToServer();
+    void do_connect(const tcp::resolver::results_type&);
+    void do_read_header();
+    void do_read_body();
+    void do_write();
+    //    std::unique_ptr<Keyboard> keyboard;
+    //    WSAData winSockData;
+    //    std::string ipAddress = "192.168.0.22";
+    //    u_short port = 54000;
 
-    std::unique_ptr<Keyboard> keyboard;
-    WSAData winSockData;
-    std::string ipAddress = "192.168.0.22";
-    u_short port = 54000;
-
-    Console console{};
-    SOCKET sock;
+    //    Console console{};
+    //    SOCKET sock;
+    boost::asio::io_context& io_context_;
+    tcp::socket socket_;
+    chat_message read_msg_;
+    chat_message_queue write_msgs_;
 };
--- windows/src/Client.cpp
+++ windows/src/Client.cpp
@@ -1,57 +1,102 @@
 #include "Client.hpp"
+#include <boost/asio.hpp>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <thread>
+#include "chat_message.hpp"
 
-#include <stdexcept>
-#include <string>
+using boost::asio::ip::tcp;
 
+typedef std::deque<chat_message> chat_message_queue;
 
-Client::Client()
+chat_client::chat_client(boost::asio::io_context& io_context, const tcp::resolver::results_type& endpoints)
+    : io_context_(io_context), socket_(io_context)
 {
-    initializeWinSock();
-
-    createSocket();
-
-    connctToServer();
-
-    auto callback = [this](unsigned asciiCode){
-        char sendBuf[] = {static_cast<char>(asciiCode)};
-        send(sock, sendBuf, 1, 0);
-    };
-
-    keyboard = std::make_unique<Keyboard>(callback);
+    do_connect(endpoints);
 }
 
-void Client::initializeWinSock()
+void chat_client::write(const chat_message& msg)
 {
-    WORD version = MAKEWORD(2, 2);
-    int wsResult = WSAStartup(version, &winSockData);
-    if (wsResult != 0)
-    {
-        throw std::runtime_error("Can't start Winsock, Err #" + std::to_string(wsResult));
-    }
+    boost::asio::post(io_context_, [this, msg]() {
+        bool write_in_progress = !write_msgs_.empty();
+        write_msgs_.push_back(msg);
+        if (!write_in_progress)
+        {
+            do_write();
+        }
+    });
 }
 
-void Client::createSocket()
+void chat_client::close()
 {
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == INVALID_SOCKET)
-    {
-        WSACleanup();
-        throw std::runtime_error("Can't create socket, Err #" + std::to_string(WSAGetLastError()));
-    }
+    boost::asio::post(io_context_, [this]() { socket_.close(); });
 }
 
-void Client::connctToServer()
+void chat_client::do_connect(const tcp::resolver::results_type& endpoints)
 {
-    sockaddr_in hint;
-    hint.sin_family = AF_INET;
-    hint.sin_port = htons(port);
-    InetPton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+    boost::asio::async_connect(socket_, endpoints, [this](boost::system::error_code ec, tcp::endpoint) {
+        if (!ec)
+        {
+            do_read_header();
+        }
+    });
+}
 
-    int connResult = connect(sock, reinterpret_cast<sockaddr*>(&hint), sizeof(hint));
-    if (connResult == SOCKET_ERROR)
-    {
-        closesocket(sock);
-        WSACleanup();
-        throw std::runtime_error("Can't connect to server, Err #" + std::to_string(WSAGetLastError()));
-    }
+void chat_client::do_read_header()
+{
+    boost::asio::async_read(
+        socket_,
+        boost::asio::buffer(read_msg_.data(), chat_message::header_length),
+        [this](boost::system::error_code ec, std::size_t /*length*/) {
+            if (!ec && read_msg_.decode_header())
+            {
+                do_read_body();
+            }
+            else
+            {
+                socket_.close();
+            }
+        });
+}
+
+void chat_client::do_read_body()
+{
+    boost::asio::async_read(
+        socket_,
+        boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
+        [this](boost::system::error_code ec, std::size_t /*length*/) {
+            if (!ec)
+            {
+                std::cout << "DOSTALO" << std::endl;
+                std::cout.write(read_msg_.body(), read_msg_.body_length());
+                std::cout << "\n";
+                do_read_header();
+            }
+            else
+            {
+                socket_.close();
+            }
+        });
+}
+
+void chat_client::do_write()
+{
+    boost::asio::async_write(
+        socket_,
+        boost::asio::buffer(write_msgs_.front().data(), write_msgs_.front().length()),
+        [this](boost::system::error_code ec, std::size_t /*length*/) {
+            if (!ec)
+            {
+                write_msgs_.pop_front();
+                if (!write_msgs_.empty())
+                {
+                    do_write();
+                }
+            }
+            else
+            {
+                socket_.close();
+            }
+        });
 }
