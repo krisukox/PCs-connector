3439bc58fb836badf5008268802a3073dc55a0a5
krisukox <krzysiula1313@gmail.com>
2019-08-19 21:54:58 +0200

    [windows] Add support for keyboard shortucuts.

--- windows/include/Keyboard.hpp
+++ windows/include/Keyboard.hpp
@@ -8,16 +8,26 @@
 #include <unordered_map>
 
 using keyId = unsigned long long;
-using asciiCode = std::array<std::byte, 2>;
+using AsciiCode = std::array<std::byte, 2>;
 
 class Keyboard : std::enable_shared_from_this<Keyboard>
 {
 public:
-    Keyboard(std::function<void(asciiCode)>&&, std::function<void()>&&);
+    Keyboard(std::function<void(AsciiCode)>&&, std::function<void()>&&);
     Keyboard() = delete;
 
+    void start();
+
 private:
-    std::function<void(asciiCode)> pressedKeyCallback;
+    std::function<void(AsciiCode)> pressedKeyCallback;
     std::function<void()> stopAppCallback;
     HHOOK keyboardHook;
+    bool isWinLPressed = false;
+    bool isCtrlPressed = false;
+    bool isShiftPressed = false;
+
+    void changeState();
+    void changeCtrl(AsciiCode asciiCode);
+    void changeShift(AsciiCode asciiCode);
+    bool hookState = true;
 };
