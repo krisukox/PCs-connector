5b9fd5937fbcdfcbefa2189dfd4694cd62cb500b
krisukox <krzysiula1313@gmail.com>
2019-09-17 19:27:43 +0200

    [linux] Add possibility to move between screens Part1 -Possibility to move cursor from windows screen to linux -Cursor at linux starts from it's last position(not depends of windows cursor)

--- linux/mouse_management/include/mouse_management/FakeMouse.hpp
+++ linux/mouse_management/include/mouse_management/FakeMouse.hpp
@@ -2,6 +2,7 @@
 
 #include <X11/Xlib.h>
 #include "IMouse.hpp"
+#include "internal_types/Point.hpp"
 
 namespace mouse_management
 {
@@ -11,13 +12,32 @@ public:
     FakeMouse(Display*);
     ~FakeMouse() override;
 
-    void onEvent(const internal_types::MouseEvent&) const override;
+    void onEvent(const internal_types::MouseEvent&) override;
 
 private:
-    void onEvent(const internal_types::MouseMoveEvent&) const;
-    void onEvent(const internal_types::MouseScrollEvent&) const;
-    void onEvent(const internal_types::MouseKeyEvent&) const;
+    void onEvent(const internal_types::MouseMoveEvent&);
+    void onEvent(const internal_types::MouseScrollEvent&);
+    void onEvent(const internal_types::MouseKeyEvent&);
+
+    class CursorGuard
+    {
+    public:
+        CursorGuard(Display*);
+        bool checkIfCursorOutOfScreen(const internal_types::MouseMoveEvent&);
+
+    private:
+        internal_types::Point getMouseXCoordinate();
+
+        Display* display;
+        Window window;
+
+        Window _w_;
+        int _i_;
+        unsigned _u_;
+        int xCoordinate, yCoordinate;
+    };
 
     Display* display;
+    CursorGuard cursorGuard;
 };
 } // namespace mouse_management
--- linux/mouse_management/src/FakeMouse.cpp
+++ linux/mouse_management/src/FakeMouse.cpp
@@ -1,24 +1,46 @@
 #include "mouse_management/FakeMouse.hpp"
 #include <X11/extensions/XTest.h>
+#include <stdexcept>
 
 namespace mouse_management
 {
-FakeMouse::FakeMouse(Display* display_) : display{display_} {}
+FakeMouse::FakeMouse(Display* _display) : display{_display}, cursorGuard{display} {}
 
 FakeMouse::~FakeMouse() = default;
 
-void FakeMouse::onEvent(const internal_types::MouseEvent& mouseEvent) const
+void FakeMouse::onEvent(const internal_types::MouseEvent& mouseEvent)
 {
     std::visit([this](const auto& event) { onEvent(event); }, mouseEvent);
 }
 
-void FakeMouse::onEvent(const internal_types::MouseMoveEvent& mouseMoveEvent) const
+void FakeMouse::onEvent(const internal_types::MouseMoveEvent& mouseMoveEvent)
 {
+    if (cursorGuard.checkIfCursorOutOfScreen(mouseMoveEvent))
+    {
+        // OUT OF SCREEN
+    }
     XTestFakeRelativeMotionEvent(display, mouseMoveEvent.deltaX, mouseMoveEvent.deltaY, CurrentTime);
     XFlush(display);
 }
 
-void FakeMouse::onEvent(const internal_types::MouseScrollEvent&) const {}
+void FakeMouse::onEvent(const internal_types::MouseScrollEvent&) {}
 
-void FakeMouse::onEvent(const internal_types::MouseKeyEvent&) const {}
+void FakeMouse::onEvent(const internal_types::MouseKeyEvent&) {}
+
+FakeMouse::CursorGuard::CursorGuard(Display* _display) : display{_display}, window{XRootWindow(display, 0)} {}
+
+internal_types::Point FakeMouse::CursorGuard::getMouseXCoordinate()
+{
+    if (!XQueryPointer(display, window, &_w_, &_w_, &xCoordinate, &yCoordinate, &_i_, &_i_, &_u_))
+    {
+        throw std::runtime_error("wrong XQueryPointer result");
+    }
+    return {static_cast<short>(xCoordinate), static_cast<short>(yCoordinate)};
+}
+
+bool FakeMouse::CursorGuard::checkIfCursorOutOfScreen(const internal_types::MouseMoveEvent& mouseMoveEvent)
+{
+    internal_types::Point newPoint = getMouseXCoordinate() + mouseMoveEvent;
+    return newPoint.x >= XWidthOfScreen(XDefaultScreenOfDisplay(display));
+}
 } // namespace mouse_management
