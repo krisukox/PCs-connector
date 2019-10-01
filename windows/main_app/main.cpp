ddfa0bd33207640104a72b60bb42314119ec477f
krisukox <krzysiula1313@gmail.com>
2019-10-01 21:04:09 +0200

    [both] Add possibility to move cursor between screens

--- windows/main_app/main.cpp
+++ windows/main_app/main.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include "app_management/App.hpp"
 
@@ -11,5 +12,9 @@ int main(int argc, char* argv[])
     {
         std::cerr << "Exception: " << e.what() << "\n";
     }
+    //    auto cos = std::byte{static_cast<std::uint8_t>(-128)};
+    //    if (cos == std::byte{0b10000000}) std::cout << "TAK" << std::endl;
+    //    int cos1 = std::to_integer<std::int8_t>(cos);
+    //    std::cout << cos1 << std::endl;
     return 0;
 }
