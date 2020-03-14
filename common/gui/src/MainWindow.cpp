cd9ef91dee542bb315be5da6d2fcd1d541c7a62e
Krzysztof Naglik <krzysiula1313@gmail.com>
2020-03-14 21:21:36 +0100

    Move GUI library to the common directory (#63)

--- common/gui/src/MainWindow.cpp
+++ common/gui/src/MainWindow.cpp
@@ -2,6 +2,7 @@
 
 #include <QCoreApplication>
 #include <QMouseEvent>
+#include <QScreen>
 #include "gui/GraphicsRectItem.h"
 #include "gui/GraphicsScene.h"
 #include "gui/MainWindow.h"
@@ -24,26 +25,62 @@ char** convertToArgv(QStringList commandArgs)
     }
     return argv;
 }
+
+const unsigned SCREEN_SIZE_MULTIPLIER = 10;
+
+const int SCREEN_WIDTH_1 = 1920;
+const int SCREEN_HEIGHT_1 = 1080;
+const int SCREEN_WIDTH_2 = 1366;
+const int SCREEN_HEIGHT_2 = 768;
+
+QSize getMasterSize()
+{
+    return qApp->screens().at(0)->size();
+}
+
+QSize getSlaveSize()
+{
+    if (qApp->screens().at(0)->size().width() == SCREEN_WIDTH_1)
+    {
+        return QSize{SCREEN_WIDTH_2, SCREEN_HEIGHT_2};
+    }
+    else
+    {
+        return QSize{SCREEN_WIDTH_1, SCREEN_HEIGHT_1};
+    }
+}
 } // namespace
 
 MainWindow::MainWindow(QWidget* parent)
-    : QMainWindow{parent}, ui{new Ui::MainWindow}, app(std::make_unique<app_management::App>())
+    : QMainWindow{parent}
+    , ui{new Ui::MainWindow}
+    , app{std::make_unique<app_management::App>()}
+    , MASTER_SIZE{getMasterSize()}
+    , SLAVE_SIZE{getSlaveSize()}
 {
     ui->setupUi(this);
 
-    auto setContactPoints = [this](const std::pair<QPointF, QPointF>& contactPoints, const QPointF& diffPoint) {};
+    auto setContactPoints = [this](const std::pair<QPointF, QPointF>& contactPoints, const QPointF& diffPoint) {
+        this->app->setContactPoints(
+            {{static_cast<short>(contactPoints.first.x() * SCREEN_SIZE_MULTIPLIER),
+              static_cast<short>(contactPoints.first.y() * SCREEN_SIZE_MULTIPLIER)},
+             {static_cast<short>(contactPoints.second.x() * SCREEN_SIZE_MULTIPLIER),
+              static_cast<short>(contactPoints.second.y() * SCREEN_SIZE_MULTIPLIER)}},
+            {static_cast<short>(diffPoint.x() * SCREEN_SIZE_MULTIPLIER),
+             static_cast<short>(diffPoint.y() * SCREEN_SIZE_MULTIPLIER)});
+    };
     auto scene = new GraphicsScene(0, 0, 598, 598, std::move(setContactPoints));
 
     ui->graphicsView->setScene(scene);
 
     GraphicsRectItem* item = new GraphicsRectItem(
-        QRectF(0, 0, MASTER_SCREEN_WIDTH / SCREEN_SIZE_MULTIPLIER, MASTER_SCREEN_HEIGHT / SCREEN_SIZE_MULTIPLIER),
+        QRectF(0, 0, MASTER_SIZE.width() / SCREEN_SIZE_MULTIPLIER, MASTER_SIZE.height() / SCREEN_SIZE_MULTIPLIER),
         GraphicsRectItem::ScreenType::master);
     item->setBrush(QBrush(Qt::green));
     item->setFlags(QGraphicsItem::ItemIsMovable);
 
     GraphicsRectItem* item2 = new GraphicsRectItem(
-        QRectF(0, 0, SLAVE_SCREEN_WIDTH / SCREEN_SIZE_MULTIPLIER, SLAVE_SCREEN_HEIGHT / SCREEN_SIZE_MULTIPLIER),
+        QRectF(0, 0, SLAVE_SIZE.width() / SCREEN_SIZE_MULTIPLIER, SLAVE_SIZE.height() / SCREEN_SIZE_MULTIPLIER),
         GraphicsRectItem::ScreenType::slave);
     item2->setBrush(QBrush(Qt::blue));
     item2->setFlags(QGraphicsItem::ItemIsMovable);
