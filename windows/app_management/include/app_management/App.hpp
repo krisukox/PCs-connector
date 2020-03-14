9ecdc85dbf6939d9d7da3df20418e44f9ff7d6bd
Krzysztof Naglik <krzysiula1313@gmail.com>
2020-03-14 20:28:44 +0100

    Introduce App interface (#62)

--- windows/app_management/include/app_management/App.hpp
+++ windows/app_management/include/app_management/App.hpp
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include "app_management/Console.hpp"
+#include "interface/IApp.hpp"
 
 namespace connection
 {
@@ -13,13 +14,15 @@ namespace app_management
 {
 class Vendor;
 
-class App
+class App : public IApp
 {
 public:
     App();
-    ~App();
+    ~App() override;
 
-    void start(int, char*[]);
+    void start(int, char*[]) override;
+    void setContactPoints(const std::pair<internal_types::Point, internal_types::Point>&, const internal_types::Point&)
+        override;
 
 private:
     void initializeVendor();
